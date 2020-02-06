1bef09b4412312370ddcf682183226ee79dcb4d4
Yurii Kochut <kochutyura@gmail.com>
2020-02-06 10:20:54 +0200

    add pages | sructure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { WorkComponent } from './pages/work/work.component';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    WorkComponent,
+    AboutComponent,
+    BlogComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
