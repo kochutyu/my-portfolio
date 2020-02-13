b0abe12e5e4ee591e0ee8e13e83089eccdc4c78e
Yura Kochut <kochutyurawork@gmail.com>
2020-02-14 00:40:35 +0200

    addupdate (work)

--- src/app/pages/work/work.component.ts
+++ src/app/pages/work/work.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IProjectInfo } from 'src/app/shared/classes/i-project-info';
+import { ITag } from 'src/app/shared/classes/i-tag';
 
 @Component({
   selector: 'app-work',
@@ -7,6 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkComponent implements OnInit {
 
+  projectInfo: IProjectInfo[] = [
+    {
+      img: '../../../assets/img/work/user.png',
+      data: 'kochutyu',
+    },
+    {
+      img: '../../../assets/img/work/like.png',
+      data: '12',
+    },
+    {
+      img: '../../../assets/img/work/date.png',
+      data: '13.02.2020',
+    },
+    {
+      img: '../../../assets/img/work/coments.png',
+      data: '4',
+    }
+  ]
+
+  tags: ITag[] = [
+    {content:'Angular'},
+    {content:'CSS'},
+    {content:'JavaScript'},
+    {content:'JQuery'},
+    {content:'Angular'},
+    {content:'CSS'},
+    {content:'JavaScript'},
+    {content:'JQuery'},
+    {content:'Angular'},
+    {content:'CSS'},
+    {content:'JavaScript'},
+    {content:'JQuery'},
+  ]
+  
   constructor() { }
 
   ngOnInit() {
