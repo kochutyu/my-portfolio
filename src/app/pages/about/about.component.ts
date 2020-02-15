d0767e4dc213c0f3c90e60ed6baca117f5ccbb25
Yura Kochut <kochutyurawork@gmail.com>
2020-02-15 10:58:02 +0200

    update (about - add view)

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ITag } from 'src/app/shared/classes/i-tag';
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+
+  slider: string[] = [
+    'https://picsum.photos/100',
+    'https://picsum.photos/200',
+    'https://picsum.photos/300',
+  ]
+
+  tags: ITag[] = [
+    {content:'CSS'},
+    {content:'HTML'},
+    {content:'JQuery'},
+    {content:'Angular'},
+  ]
 
   constructor() { }
 
